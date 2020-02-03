8d34b5a8db16d18dd7af2fb6ca2ed1597538e5c2
James McGhee <59738821+jemcghee3@users.noreply.github.com>
2020-02-03 12:06:12 +0000

    Add files via upload

---
+++ ch12_e4.py
@@ -0,0 +1,29 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the paragraph tags
+tags = soup('p')
+count = 0
+for tag in tags:
+    count +=1
+
+print('The number of paragraph tags is: %s' % count)
+
+
